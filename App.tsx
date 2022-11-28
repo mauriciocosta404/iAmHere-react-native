238f692e910db385d73e2f253be8dd110deefaa2
menezescosta <mc611641@gmail.com>
2022-11-28 21:43:37 +0000

    feat:adicionando o nome do evento

--- App.tsx
+++ App.tsx
@@ -2,8 +2,21 @@ import { Text,View } from "react-native"
 
 export default function App(){
   return(
-    <View>
-        <Text>react native</Text>
+    <View style={{
+      flex:1,
+      backgroundColor:'#131016',
+      padding:24
+    }}>
+      <Text style={{  
+        color:'#fdfcfe',
+        fontSize:24,
+        fontWeight:'bold',
+        marginTop:48
+      }}>Nome do Evento</Text>
+      <Text style={{
+        color:'#6b6b6b',
+        fontSize:16
+      }}>Sexta-feira, 4 de setembro</Text>
     </View>
   )
 }
