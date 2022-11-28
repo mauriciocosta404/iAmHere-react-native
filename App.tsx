0d4d712c6161d4b73e29b93dfd0aca4e44fe6a73
menezescosta <mc611641@gmail.com>
2022-11-28 18:47:46 +0000

    feat:configurando o projecto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text,View } from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  return(
+    <View>
+        <Text>react native</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
