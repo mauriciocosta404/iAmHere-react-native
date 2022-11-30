f904efd3567f96ecc20a883e902892dad79cc2f5
menezescosta <mc611641@gmail.com>
2022-11-30 22:54:07 +0000

    feat:adicionando o form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,16 +11,18 @@ export default function Home(){
     <View style={Styles.container}>
       <Text style={Styles.eventName}>Nome do Evento</Text>
       <Text style={Styles.eventDate}>Sexta-feira, 4 de setembro</Text>
-      <TextInput 
-        style={Styles.input} 
-        placeholder="Nome do participante" 
-        placeholderTextColor='#6b6b6b'
-    />
-    <TouchableOpacity style={Styles.button} onPress={handlePartcipantAdd}>
-        <Text style={Styles.buttonText}> 
+      <View  style={Styles.form} >
+        <TextInput 
+          style={Styles.input} 
+          placeholder="Nome do participante" 
+          placeholderTextColor='#6b6b6b'
+        />
+        <TouchableOpacity style={Styles.button} onPress={handlePartcipantAdd}>
+          <Text style={Styles.buttonText}> 
             +
-        </Text>
-    </TouchableOpacity>
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,40 +1,48 @@
 import { StyleSheet } from "react-native"
 
 export const Styles=StyleSheet.create({
-    container:{
-      flex:1,
-      backgroundColor:'#131016',
-      padding:24
+  container:{
+    flex:1,
+    backgroundColor:'#131016',
+    padding:24
+  },
+  eventName: {
+    color:'#fdfcfe',
+    fontSize:24,
+    fontWeight:'bold',
+    marginTop:48
+  },
+  eventDate:{
+    color:'#6b6b6b',
+    fontSize:16
+  },
+  input:{
+    height:46,
+    backgroundColor:'#1f1e25',
+    borderRadius:3,
+    color:'#fdfcfe',
+    marginRight:7,
+    padding:10,
+    fontSize:13,
+    flex:1
+  },
+  buttonText:{
+    color:'#fff',
+    fontSize:36,
+    textAlign:'center',
+    alignSelf:'center',
+  },
+  button:{
+    height:46,
+    width:46,
+    borderRadius:5,
+    backgroundColor:'#31cf67'
     },
-    eventName: {
-      color:'#fdfcfe',
-      fontSize:24,
-      fontWeight:'bold',
-      marginTop:48
-    },
-    eventDate:{
-      color:'#6b6b6b',
-      fontSize:16
-    },
-    input:{
-        height:46,
-        backgroundColor:'#1f1e25',
-        borderRadius:3,
-        color:'#fdfcfe',
-        marginTop:7,
-        padding:10,
-        fontSize:13,
-    },
-    buttonText:{
-        color:'#fff',
-        fontSize:36,
-        textAlign:'center',
-        alignSelf:'center',
-    },
-    button:{
-        height:46,
-        width:46,
-        borderRadius:5,
-        backgroundColor:'#31cf67'
-    }
-  });
+  form:{
+    marginTop:30,
+    width:'100%',
+    flexDirection:'row',  
+    alignItems:'center',
+    marginBottom:30,
+  }
+});
