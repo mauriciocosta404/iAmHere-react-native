a78f95d192a960dbe5685a4af2659a3caa45604e
menezescosta <mc611641@gmail.com>
2022-12-17 00:29:23 +0100

    feat: trabalhando com o statusbar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
+import { StatusBar } from "expo-status-bar"
 import Home from "./src/screens/Home"
 
 export default function App(){
   return(
-    <Home />
-      
+    <>
+      <StatusBar 
+        backgroundColor="transparent"
+        translucent 
+      />
+      <Home />
+    </>
   )
 }
 
